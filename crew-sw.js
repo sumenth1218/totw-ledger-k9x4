1e4468cc87c9d429ef2fc4b6b37d05b513959b73
sumenth1218 <119860877+sumenth1218@users.noreply.github.com>
2026-07-21 01:24:09 +0530

    Add files via upload

--- crew-sw.js
+++ crew-sw.js
@@ -1,5 +1,5 @@
-const CACHE='totw-crew-v1';
-const SHELL=['./crew.html','./crew-manifest.webmanifest','./crew-icon.svg','./crew-icon-512.png','./crew-icon-180.png'];
+const CACHE='totw-crew-v2';
+const SHELL=['./crew.html','./crew-manifest.webmanifest','./totw-logo-black.png','./totw-logo-white.png','./totw-logo-square.jpg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
