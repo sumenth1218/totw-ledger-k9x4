6db4ea186314a22cc44cc6c3d9c67bfbc3cd0182
sumenth1218 <119860877+sumenth1218@users.noreply.github.com>
2026-08-09 19:14:20 +0530

    Add files via upload

--- crew-sw.js
+++ crew-sw.js
@@ -1,4 +1,4 @@
-const CACHE='tale-of-two-crew-v16-ledger-cache-isolation';
+const CACHE='tale-of-two-crew-v17-geofence-override';
 const SHELL=['./crew.html','./crew-manifest.webmanifest','./totw-logo-black.png','./totw-logo-white.png','./totw-logo-square.jpg','./crew-icon-180.png','./crew-icon-512.png'];
 const CREW_FILES=new Set(SHELL.map(path=>new URL(path,self.location.href).pathname));
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
