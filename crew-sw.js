c6586b0d4c17eaf220f65a39bfa91216645fb3c0
sumenth1218 <119860877+sumenth1218@users.noreply.github.com>
2026-08-14 07:15:29 +0530

    Add files via upload

--- crew-sw.js
+++ crew-sw.js
@@ -1,7 +1,7 @@
-const CACHE='tale-of-two-crew-v19-audit-fix';
+const CACHE='tale-of-two-crew-v20-round2';
 const SHELL=['./crew.html','./crew-manifest.webmanifest','./totw-logo-black.png','./totw-logo-white.png','./totw-logo-square.jpg','./crew-icon-180.png','./crew-icon-512.png'];
 const CREW_FILES=new Set(SHELL.map(path=>new URL(path,self.location.href).pathname));
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>Promise.all(SHELL.map(url=>fetch(url,{cache:'reload'}).then(r=>r&&r.ok?cache.put(url,r):null).catch(()=>null)))).then(()=>self.skipWaiting()).catch(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
   const url=new URL(event.request.url);
